f7e53d9f12a407b02148b797818dd69db02a5543
Min-soo Park <rinirihiriro@gmail.com>
2014-11-14 13:02:53 +0900

    delete all

--- client.cpp
+++ client.cpp
@@ -9,47 +9,48 @@
 
 int main(const int argc, const char * const * cont argv)
 {
-    if (argc != 4)
-    {
-        printf("USAGE : %s <IP> <PORT> <NICKNAME>", argv[0]);
-        return -1;
-    }
+	if (argc != 4)
+	{
+		printf("USAGE : %s <IP> <PORT> <NICKNAME>", argv[0]);
+		return -1;
+	}
 
-    int ret = -1;
-    int clientSock;
-    struct sockaddr_in serverAddr;
+	int ret = -1;
+	int clientSock;
+	struct sockaddr_in serverAddr;
 
-    if ((clientSock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        goto leave;
-    }
+	if ((clientSock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket");
+		goto leave;
+	}
 
-    unsigned int ip = inet_addr(argv[1]);
-    unsigned short port = atoi(argv[2]);
+	unsigned int ip = inet_addr(argv[1]);
+	unsigned short port = atoi(argv[2]);
 
-    if (ip == INADDR_NONE || ip == INADDR_ANY)
-    {
-    }
+	if (ip == INADDR_NONE || ip == INADDR_ANY)
+	{
+		perror("invalid ip")
+	}
 
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = inet_addr();
-    serverAddr.sin_port = htons();
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = inet_addr(ip);
+	serverAddr.sin_port = htons(port);
 
-    if ((ret = connect(clientSock,(struct sockaddr*)&serverAddr,
-                       sizeof(serverAddr)))) {
-        perror("connect");
-        goto error;
-    }
+	if ((ret = connect(clientSock,(struct sockaddr*)&serverAddr,
+					   sizeof(serverAddr)))) {
+		perror("connect");
+		goto error;
+	}
 
-    if ((ret = send(clientSock, WRITE_DATA, sizeof(WRITE_DATA), 0)) <= 0) {
-        perror("send");
-        ret = -1;
-    } else
-        printf("Wrote '%s' (%d Bytes)\n", WRITE_DATA, ret);
+	if ((ret = send(clientSock, WRITE_DATA, sizeof(WRITE_DATA), 0)) <= 0) {
+		perror("send");
+		ret = -1;
+	} else
+		printf("Wrote '%s' (%d Bytes)\n", WRITE_DATA, ret);
 
 error:
-    close(clientSock);
+	close(clientSock);
 leave:
-    return ret;
+	return ret;
 }
 
